424a4a2b09077a7269b306ba2bad01140bc43336
Juan191101 <ruartejuancruz@gmail.com>
2023-09-20 21:00:14 -0300

    a[

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -61,7 +61,7 @@ const AboutSection = () => {
   return (
     <section id="about" className='text-white mt-20'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 '>
-        <Image src={"/images/about.jpg"} width={400} height={400} alt="image" className='rounded-3xl'></Image>
+        <Image src={"/images/about.jpg"} width={400} height={400} alt="image" className='rounded-full'></Image>
         <div  className='mt-4 md:mt-0 text-left flex flex-col h-full'>
             <h2 className='text-4xl font-bold text-white mb-4'>
                 Sobre mi
